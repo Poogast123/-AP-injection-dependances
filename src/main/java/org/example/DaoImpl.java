cee3ae0aff32420978eafee7362378f573f0dcd1
kharifi oussama <oussamakharifi2003@gmail.com>
2025-02-24 10:05:49 +0000

    instanciation statique

--- src/main/java/org/example/DaoImpl.java
+++ src/main/java/org/example/DaoImpl.java
@@ -3,6 +3,6 @@ package org.example;
 public class DaoImpl implements IDao {
     @Override
     public double getData() {
-        return Math.random() * 100; // Simule une valeur
+        return Math.random() * 100;
     }
 }
