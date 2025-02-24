9362beaa28eb3f57d72d2f1e4223ef20d457dff6
kharifi oussama <oussamakharifi2003@gmail.com>
2025-02-24 10:51:38 +0000

    instanciation dynamique

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,19 @@
 package org.example;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args) {
-        DaoImpl dao = new DaoImpl();
-        MetierImpl metier = new MetierImpl();
-        metier.setDao(dao);
+    public static void main(String[] args) throws Exception {
+        // Charger le fichier de config
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("config.txt"));
+
+        // Instancier les classes dynamiquement
+        IDao dao = (IDao) Class.forName(properties.getProperty("dao")).newInstance();
+        MetierImpl metier = (MetierImpl) Class.forName(properties.getProperty("metier")).newInstance();
+
+        metier.setDao(dao); // Injection dynamique
 
         System.out.println("Résultat : " + metier.calcul());
     }
